ef70abaf897de415d7547c280c413fb9d2ffac65
Xiamu-ssr <77220168+Xiamu-ssr@users.noreply.github.com>
2023-05-09 17:58:11 +0800

    Update set-cpu-freq.sh

--- set-cpu-freq.sh
+++ set-cpu-freq.sh
@@ -5,7 +5,16 @@ num=$2
 hostfile="hostfile.txt"
 
 if [[ $server == "all" ]]; then
-    parallel-ssh -h $hostfile -i -t 0 "cpupower -c all frequency-set -u $num > /dev/null"
+    if [[ $num == "powersave" ]]; then
+        echo "powersave"
+        parallel-ssh -h $hostfile -i -t 0 "cpupower -c all frequency-set -g powersave > /dev/null"
+    elif [[ $num == "performance" ]]; then 
+        echo "performance"
+        parallel-ssh -h $hostfile -i -t 0 "cpupower -c all frequency-set -g performance > /dev/null"
+    else
+        echo $num
+        parallel-ssh -h $hostfile -i -t 0 "cpupower -c all frequency-set -u $num > /dev/null"
+    fi
 else
     ssh root@$server "cpupower -c all frequency-set -u $num > /dev/null"
 fi
